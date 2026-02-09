3e2fd44e74473044e1c4e50fdec230b244dc18b0
Aki1104 <steevenparubrub@gmail.com>
2026-02-09 11:31:42 +0800

    Refactor job-related pages with new layout and updated styles; replace font with Manrope and Space Grotesk; enhance user experience with additional input fields and tips; update Firebase configuration.

--- web/js/firebase-config.js
+++ web/js/firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyD6sg_ikwqMYipyYD02LI4Ml4K_FCjeuJo",
+  authDomain: "ic-opportunity.firebaseapp.com",
+  projectId: "ic-opportunity",
+  storageBucket: "ic-opportunity.firebasestorage.app",
+  messagingSenderId: "328046431673",
+  appId: "1:328046431673:web:0eb9ab106aa42feb483a68",
+  measurementId: "G-XPMF833BKK"
 };
